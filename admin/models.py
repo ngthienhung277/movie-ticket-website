e4a85e14b076edf6ba228e04d4140b99d074d3d3
Duy Nguyen <nguyenhoanganhduy@protonmail.com>
2019-04-22 23:14:03 +0700

    Add models

--- admin/models.py
+++ admin/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class Director(models.Model):
+    name = models.CharField(max_length=255)
+    summary = models.TextField()
+    birthdate = models.DateField()
+    nationality = models.CharField(max_length=255)
+    bio = models.TextField()
+    # TODO: Pictures
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+    summary = models.TextField()
+    birthdate = models.DateField()
+    nationality = models.CharField(max_length=255)
+    bio = models.TextField()
+    # TODO: Pictures
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    running_time = models.PositiveSmallIntegerField()
+    country = models.CharField(max_length=255)
+    directors = models.ManyToManyField(Director)
+    actors = models.ManyToManyField(Actor)
+    genre = models.ManyToManyField(Genre)
+    production_company = models.CharField(max_length=255)
+    release_date = models.DateField()
+    content = models.TextField()
+    # TODO: Trailer and Picture
+
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    region = models.CharField(max_length=255)
+    phone = models.CharField(max_length=10, min_length=10)
+    number_screening_room = models.PositiveSmallIntegerField()
+    number_seats = models.PositiveSmallIntegerField()
+    # TODO: Status of cinema?
+
+class ScreeningRoom(models.Model):
+    # TODO: Combined unique constraints
+    room_number = models.PositiveSmallIntegerField()
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    # TODO: status
+    number_seats = models.PositiveSmallIntegerField()
+    number_rows = models.PositiveSmallIntegerField()
+
+
+class Seat(models.Model):
+    seat_number = models.PositiveSmallIntegerField()
+    seat_row = models.CharField(max_length=1)
+    screening_room = models.ForeignKey(ScreeningRoom, on_delete=models.CASCADE)
+
+
+class MovieSchedule(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE)
+    screening_room = models.ForeignKey(ScreeningRoom, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+# Reference: https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField()
+    gender = models.CharField(max_length=1, choices=(('F', 'Female'), ('M', 'Male')))
+    phone = models.CharField(max_length=10)
+    street_address = models.CharField(max_length=255)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
